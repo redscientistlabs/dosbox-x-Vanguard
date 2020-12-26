ecd2666a00f2b876ac831bb5c03af5bebf023671
Christian <christianaltermatt@gmail.com>
2020-12-26 09:20:36 -0700

    add stub for possible glide passthrough memory domain. Maybe we could find a way to read the pagehandler of the passthrough? There's read and write functions in glide.cpp

--- src/Vanguard/VanguardClient.cpp
+++ src/Vanguard/VanguardClient.cpp
@@ -368,7 +368,18 @@ public:
 //    virtual void PokeByte(long long addr, unsigned char val);
 //};
 //public
-//ref class Voodoo : RTCV::CorruptCore::IMemoryDomain {
+//ref class VoodooEmulation : RTCV::CorruptCore::IMemoryDomain {
+//public:
+//    property System::String^ Name { virtual System::String^ get(); }
+//    property long long Size { virtual long long get(); }
+//    property int WordSize { virtual int get(); }
+//    property bool BigEndian { virtual bool get(); }
+//    virtual unsigned char PeekByte(long long addr);
+//    virtual array<unsigned char>^ PeekBytes(long long address, int length);
+//    virtual void PokeByte(long long addr, unsigned char val);
+//};
+//public
+//ref class GlidePassThru : RTCV::CorruptCore::IMemoryDomain {
 //public:
 //    property System::String^ Name { virtual System::String^ get(); }
 //    property long long Size { virtual long long get(); }
@@ -801,6 +812,7 @@ void VanguardClient::LoadRom(String^ filename) {
 
 bool VanguardClient::LoadState(std::string filename) {
     //StepActions::ClearStepBlastUnits();
+    
     RtcClock::ResetCount();
     stateLoading = true;
     UnmanagedWrapper::VANGUARD_LOADSTATE(filename);
